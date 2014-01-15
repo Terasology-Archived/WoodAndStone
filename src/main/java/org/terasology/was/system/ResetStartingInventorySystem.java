5ebe9b167b8ceab515ca5b5dd8e1ab6f808a2bbb
MarcinSc <marcins78@gmail.com>
2014-01-15 02:15:19 +0000

    Wiping inventory upon player spawn, rather than preventing addition of items by the default system.

--- src/main/java/org/terasology/was/system/ResetStartingInventorySystem.java
+++ src/main/java/org/terasology/was/system/ResetStartingInventorySystem.java
@@ -1,16 +1,22 @@
 package org.terasology.was.system;
 
-import org.terasology.core.logic.BeforeGivingStartingInventory;
 import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.event.EventPriority;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.ComponentSystem;
+import org.terasology.entitySystem.systems.In;
 import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.logic.inventory.SlotBasedInventoryManager;
+import org.terasology.logic.players.event.OnPlayerSpawnedEvent;
 
 /**
  * @author Marcin Sciesinski <marcins78@gmail.com>
  */
 @RegisterSystem
 public class ResetStartingInventorySystem implements ComponentSystem {
+    @In
+    private SlotBasedInventoryManager manager;
+
     @Override
     public void initialise() {
     }
@@ -19,8 +25,10 @@ public class ResetStartingInventorySystem implements ComponentSystem {
     public void shutdown() {
     }
 
-    @ReceiveEvent
-    public void resetStartingInventory(BeforeGivingStartingInventory event, EntityRef character) {
-        event.consume();
+    @ReceiveEvent(priority = EventPriority.PRIORITY_TRIVIAL)
+    public void resetStartingInventory(OnPlayerSpawnedEvent event, EntityRef character) {
+        for (int i = 0; i < manager.getNumSlots(character); i++) {
+            manager.destroyItem(character, manager.getItemInSlot(character, i));
+        }
     }
 }
