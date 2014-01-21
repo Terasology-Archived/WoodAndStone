2464ca92f3389b14d85cfab499156409882b84ba
MarcinSc <marcins78@gmail.com>
2014-01-21 03:18:01 +0000

    Fixing issues after integrating with latest build.

--- src/main/java/org/terasology/crafting/event/CraftInHandButton.java
+++ src/main/java/org/terasology/crafting/event/CraftInHandButton.java
@@ -25,6 +25,6 @@ import org.terasology.input.RegisterBindButton;
  * @author Marcin Sciesinski <marcins78@gmail.com>
  */
 @RegisterBindButton(id = "craftInHand", description = "Craft in hand")
-@DefaultBinding(type = InputType.KEY, id = Keyboard.KEY_C)
+@DefaultBinding(type = InputType.KEY, id = Keyboard.KEY_G)
 public class CraftInHandButton extends BindButtonEvent {
 }
