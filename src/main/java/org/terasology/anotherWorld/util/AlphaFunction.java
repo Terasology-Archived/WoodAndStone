7fe3c140c359539d83a21ae11f3c96c6c9788e9c
MarcinSc <marcins78@gmail.com>
2014-01-26 14:36:56 +0800

    Alpha function documentation.

--- src/main/java/org/terasology/anotherWorld/util/AlphaFunction.java
+++ src/main/java/org/terasology/anotherWorld/util/AlphaFunction.java
@@ -16,6 +16,13 @@
 package org.terasology.anotherWorld.util;
 
 /**
+ * Alpha function translates one value into another. The input value is more-or-less in range of 0-1, output should
+ * be in the same range.
+ * See http://easings.net/ for more details and examples.
+ * In generators used mainly for modifying the uniform noise function. This allows to transform a uniform 0-1 space
+ * of possible values into more controlled one, for example - allows to modify so that low values appear more often
+ * than higher values.
+ *
  * @author Marcin Sciesinski <marcins78@gmail.com>
  */
 public interface AlphaFunction {
