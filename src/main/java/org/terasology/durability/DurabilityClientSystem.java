59e8a5848b874828b6c3c2dade3f7fc2453f4a8f
MarcinSc <marcins78@gmail.com>
2014-02-10 14:19:20 +0000

    Rendering durability with NUI.

--- src/main/java/org/terasology/durability/DurabilityClientSystem.java
+++ src/main/java/org/terasology/durability/DurabilityClientSystem.java
@@ -15,12 +15,22 @@
  */
 package org.terasology.durability;
 
+import org.terasology.asset.AssetUri;
+import org.terasology.asset.Assets;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.ComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.math.Rect2i;
+import org.terasology.math.Vector2i;
+import org.terasology.rendering.assets.texture.Texture;
+import org.terasology.rendering.assets.texture.TextureUtil;
 import org.terasology.rendering.gui.events.UIItemIconRendered;
+import org.terasology.rendering.nui.Canvas;
+import org.terasology.rendering.nui.layers.ingame.inventory.InventoryCellRendered;
+
+import java.awt.*;
 
 import static org.lwjgl.opengl.GL11.GL_QUADS;
 import static org.lwjgl.opengl.GL11.glBegin;
@@ -39,6 +49,32 @@ public class DurabilityClientSystem implements ComponentSystem {
 
     @Override
     public void shutdown() {
+    }
+
+    @ReceiveEvent(components = {DurabilityComponent.class})
+    public void drawDurabilityBar(InventoryCellRendered event, EntityRef entity) {
+        Canvas canvas = event.getCanvas();
+
+        Vector2i size = canvas.size();
+
+        int minX = (int) (size.x * 0.1f);
+        int maxX = (int) (size.x * 0.9f);
+
+        int minY = (int) (size.y * 0.8f);
+        int maxY = (int) (size.y * 0.9f);
+
+        DurabilityComponent durability = entity.getComponent(DurabilityComponent.class);
+        float durabilityPercentage = durability.durability / durability.maxDurability;
+
+        if (durabilityPercentage != 1f) {
+            AssetUri backgroundTexture = TextureUtil.getTextureUriForColor(Color.WHITE);
+            AssetUri barTexture = TextureUtil.getTextureUriForColor(new Color(1 - durabilityPercentage, durabilityPercentage, 0));
+
+            canvas.drawTexture(Assets.get(backgroundTexture, Texture.class), Rect2i.createFromMinAndMax(minX, minY, maxX, maxY));
+            int durabilityBarLength = (int) (durabilityPercentage * (maxX - minX - 2));
+            int durabilityBarHeight = maxY - minY - 2;
+            canvas.drawTexture(Assets.get(barTexture, Texture.class), Rect2i.createFromMinAndSize(minX + 1, minY + 1, durabilityBarLength, durabilityBarHeight));
+        }
     }
 
     @ReceiveEvent(components = {DurabilityComponent.class})
