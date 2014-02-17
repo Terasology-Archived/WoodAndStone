53b4586bb54bafe085d36cf4c927358667e7ece9
MarcinSc <marcins78@gmail.com>
2014-02-17 15:28:38 +0000

    Ensure each seeded items is included once only.

--- src/main/java/org/terasology/was/system/SeedingFruitsRecipe.java
+++ src/main/java/org/terasology/was/system/SeedingFruitsRecipe.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.terasology.was.system;
 
 import org.terasology.crafting.component.CraftInHandRecipeComponent;
@@ -14,10 +29,12 @@ import org.terasology.world.block.BlockManager;
 import org.terasology.world.block.family.BlockFamily;
 import org.terasology.world.block.items.BlockItemFactory;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Marcin Sciesinski <marcins78@gmail.com>
@@ -32,11 +49,16 @@ public class SeedingFruitsRecipe implements CraftInHandRecipe {
         }
 
         List<CraftInHandResult> results = new LinkedList<>();
+        Set<String> usedFruits = new HashSet<>();
 
         for (int i = 0; i < slotCount; i++) {
             Prefab prefab = InventoryUtils.getItemAt(character, i).getParentPrefab();
             if (prefab != null && prefab.getURI().getNormalisedModuleName().equals("plantpack")
-                    && prefab.getURI().getNormalisedAssetName().endsWith("fruit")) {
+                    && prefab.getURI().getNormalisedAssetName().endsWith("fruit")
+                    && !usedFruits.contains(prefab.getURI().getNormalisedAssetName())) {
+                String assetName = prefab.getURI().getNormalisedAssetName();
+                String fruitName = assetName.substring(0, assetName.length() - 5);
+                usedFruits.add(fruitName);
                 results.add(new Result(character, i, knifeSlot));
             }
         }
