6690d31de78a3189966723bfc9399e034bbe4e7a
MarcinSc <marcins78@gmail.com>
2014-02-17 01:48:16 +0000

    Journal now only optionally can contain the timestamp.

--- src/main/java/org/terasology/was/system/WoodAndStoneJournalIntegration.java
+++ src/main/java/org/terasology/was/system/WoodAndStoneJournalIntegration.java
@@ -64,30 +64,30 @@ public class WoodAndStoneJournalIntegration extends BaseComponentSystem {
 
         chapterHandler.registerJournalEntry("1", firstEntry);
 
-        chapterHandler.registerJournalEntry("2", "Excellent! I got a hammer, I should be able to sharpen one of the stones " +
+        chapterHandler.registerJournalEntry("2", true, "Excellent! I got a hammer, I should be able to sharpen one of the stones " +
                 "and using the same technique as for the hammer, create a Crude Hammer! I can also dig stone with it to get stones more " +
                 "easily. It's not perfect but will have to do until I get my hands on a pick.");
 
-        chapterHandler.registerJournalEntry("3", "Ouch! That thing is sharp! I should watch out when handling it. " +
+        chapterHandler.registerJournalEntry("3", true, "Ouch! That thing is sharp! I should watch out when handling it. " +
                 "Now I should be able to cut some of the trees and get Wood Logs.");
 
-        chapterHandler.registerJournalEntry("4", "These are big, there is no way I could handle them in my hands. " +
+        chapterHandler.registerJournalEntry("4", true, "These are big, there is no way I could handle them in my hands. " +
                 "I have to build a place where I could work on them. I should place two of the logs on the ground next to each other " +
                 "and then place my Axe on it (right-click your axe on the top face of one of the logs).");
 
-        chapterHandler.registerJournalEntry("5", "Now I can work on the logs (to open the interface, press 'E' while pointing " +
+        chapterHandler.registerJournalEntry("5", true, "Now I can work on the logs (to open the interface, press 'E' while pointing " +
                 "on the station).\n\nI can store some ingredients in the left-top corner of the station and my axe in the bottom-center " +
                 "of the station. It's very crude and won't let me do much, but once I gather 10 Wood Planks I should be able to upgrade it. " +
                 "(to upgrade place the ingredients into lower-left corner of the interface and press the 'Upgrade' button)");
 
-        chapterHandler.registerJournalEntry("6", "Finally I can make something more useful than just planks. " +
+        chapterHandler.registerJournalEntry("6", true, "Finally I can make something more useful than just planks. " +
                 "Not only that, but I can also create planks more efficiently! Quality of the workspace speaks for itself.\n\n" +
                 "But I still can't make any tools. Hard to make it just out of wood, haha. I should probably find a good place " +
                 "to work on stone materials. I should make two tables using planks and sticks, but axe is not enough to do that, " +
                 "I will also need hammer (place it next to your axe in the station).\n\nOnce I get the tables I should place them " +
                 "on the ground next to each other and put my hammer on top of one of them (same as with axe before).");
 
-        chapterHandler.registerJournalEntry("7", "Now! I should be able to sharpen a stone and use it to attach another " +
+        chapterHandler.registerJournalEntry("7", true, "Now! I should be able to sharpen a stone and use it to attach another " +
                 "stone directly to a stick to make a hammer without binding it with twigs to strengthen the connection. " +
                 "I can use the same technique to create other tools. These stones are much more sturdy than the ones I've been using so far.");
 
