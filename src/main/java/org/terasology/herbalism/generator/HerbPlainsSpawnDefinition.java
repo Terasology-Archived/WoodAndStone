0eec4e91c3c69e2683eadbd8eb48b2b402b88209
MarcinSc <marcins78@gmail.com>
2014-03-07 16:45:06 +0000

    Generating and dropping of herbs.

---
+++ src/main/java/org/terasology/herbalism/generator/HerbPlainsSpawnDefinition.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2014 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.herbalism.generator;
+
+import org.terasology.anotherWorld.coreBiome.ForestBiome;
+import org.terasology.anotherWorld.coreBiome.PlainsBiome;
+import org.terasology.anotherWorld.decorator.BlockCollectionPredicate;
+import org.terasology.gf.PlantType;
+import org.terasology.gf.generator.StaticBlockFloraSpawnDefinition;
+import org.terasology.was.generator.Blocks;
+import org.terasology.world.block.BlockUri;
+import org.terasology.world.generator.plugin.RegisterPlugin;
+
+import java.util.Arrays;
+
+/**
+ * @author Marcin Sciesinski <marcins78@gmail.com>
+ */
+@RegisterPlugin
+public class HerbPlainsSpawnDefinition extends StaticBlockFloraSpawnDefinition {
+    public HerbPlainsSpawnDefinition() {
+        super(PlantType.GRASS, PlainsBiome.ID, 0.5f, 0.3f, "Core:Herb",
+                Arrays.asList(
+                        new BlockUri("WoodAndStone:Herb1"),
+                        new BlockUri("WoodAndStone:Herb2"),
+                        new BlockUri("WoodAndStone:Herb6"),
+                        new BlockUri("WoodAndStone:Herb7")),
+                new BlockCollectionPredicate(Blocks.getBlock("Core:Grass")), null);
+    }
+}
---
+++ src/main/java/org/terasology/herbalism/system/GeneratedHerbDropSystem.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2014 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.herbalism.system;
+
+import org.terasology.entitySystem.entity.EntityManager;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.prefab.Prefab;
+import org.terasology.entitySystem.prefab.PrefabManager;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterMode;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.genome.breed.BiodiversityGenerator;
+import org.terasology.genome.breed.mutator.VocabularyGeneMutator;
+import org.terasology.genome.component.GenomeComponent;
+import org.terasology.genome.system.GenomeManager;
+import org.terasology.herbalism.Herbalism;
+import org.terasology.herbalism.component.GeneratedHerbComponent;
+import org.terasology.logic.common.DisplayNameComponent;
+import org.terasology.logic.health.DoDestroyEvent;
+import org.terasology.logic.inventory.PickupBuilder;
+import org.terasology.logic.inventory.action.GiveItemAction;
+import org.terasology.logic.location.LocationComponent;
+import org.terasology.math.TeraMath;
+import org.terasology.math.Vector2i;
+import org.terasology.physics.events.ImpulseEvent;
+import org.terasology.registry.In;
+import org.terasology.utilities.random.FastRandom;
+import org.terasology.utilities.random.Random;
+import org.terasology.world.WorldProvider;
+import org.terasology.world.block.entity.CreateBlockDropsEvent;
+import org.terasology.world.block.entity.damage.BlockDamageModifierComponent;
+import org.terasology.world.block.items.BlockItemFactory;
+
+import javax.vecmath.Vector3f;
+
+@RegisterSystem(value = RegisterMode.AUTHORITY)
+public class GeneratedHerbDropSystem extends BaseComponentSystem {
+    @In
+    private WorldProvider worldProvider;
+    @In
+    private EntityManager entityManager;
+    @In
+    private GenomeManager genomeManager;
+
+    private PickupBuilder pickupBuilder;
+    private Random random;
+
+    @Override
+    public void preBegin() {
+        pickupBuilder = new PickupBuilder();
+        random = new FastRandom();
+    }
+
+    @ReceiveEvent
+    public void whenBlockDropped(CreateBlockDropsEvent event, EntityRef blockEntity, GeneratedHerbComponent component) {
+        event.consume();
+    }
+
+    @ReceiveEvent
+    public void onDestroyed(DoDestroyEvent event, EntityRef entity, GeneratedHerbComponent herbComp, LocationComponent locationComp) {
+        BlockDamageModifierComponent blockDamageModifierComponent = event.getDamageType().getComponent(BlockDamageModifierComponent.class);
+        float chanceOfBlockDrop = 1;
+
+        if (blockDamageModifierComponent != null) {
+            chanceOfBlockDrop = 1 - blockDamageModifierComponent.blockAnnihilationChance;
+        }
+
+        if (random.nextFloat() < chanceOfBlockDrop) {
+            final String herbBaseGenome = herbComp.herbBaseGenome;
+            final Vector3f position = locationComp.getWorldPosition();
+
+            BiodiversityGenerator generator = new BiodiversityGenerator(worldProvider.getSeed(), 0, new VocabularyGeneMutator("ABCD"), herbBaseGenome,
+                    3, 0.005f);
+            final String generatedGenes = generator.generateGenes(new Vector2i(TeraMath.floorToInt(position.x + 0.5f), TeraMath.floorToInt(position.z + 0.5f)));
+
+            EntityRef herb = entityManager.create("WoodAndStone:HerbBase");
+
+            GenomeComponent genomeComponent = new GenomeComponent();
+            genomeComponent.genomeId = "Herbalism:herb";
+            genomeComponent.genes = generatedGenes;
+            herb.addComponent(genomeComponent);
+
+            final String herbName = genomeManager.getGenomeProperty(herb, Herbalism.NAME_PROPERTY, String.class);
+            DisplayNameComponent displayName = new DisplayNameComponent();
+            displayName.name = herbName;
+            herb.addComponent(displayName);
+
+            if (shouldDropToWorld(event, blockDamageModifierComponent, herb)) {
+                createDrop(herb, locationComp.getWorldPosition(), false);
+            }
+        }
+    }
+
+    private boolean shouldDropToWorld(DoDestroyEvent event, BlockDamageModifierComponent blockDamageModifierComponent, EntityRef dropItem) {
+        return blockDamageModifierComponent == null || !blockDamageModifierComponent.directPickup
+                || !giveItem(event.getInstigator(), dropItem);
+    }
+
+    private boolean giveItem(EntityRef instigator, EntityRef dropItem) {
+        GiveItemAction giveEvent = new GiveItemAction(instigator, dropItem);
+        instigator.send(giveEvent);
+        return giveEvent.isConsumed();
+    }
+
+    private void createDrop(EntityRef item, Vector3f location, boolean applyMovement) {
+        EntityRef pickup = pickupBuilder.createPickupFor(item, location, 60, true);
+        if (applyMovement) {
+            pickup.send(new ImpulseEvent(random.nextVector3f(30.0f)));
+        }
+    }
+
+}
