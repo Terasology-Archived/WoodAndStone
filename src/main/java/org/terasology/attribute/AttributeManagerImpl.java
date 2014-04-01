a5f10a2712625e4efc22ca631a7e4fea0f5092a9
MarcinSc <marcins78@gmail.com>
2014-04-01 15:36:13 +0100

    Introducing attributes and their impact on damage dealt (strength) and speed (agility).

---
+++ src/main/java/org/terasology/attribute/AttributeManagerImpl.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2014 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.attribute;
+
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.registry.Share;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RegisterSystem
+@Share(value = {AttributeManager.class})
+public class AttributeManagerImpl extends BaseComponentSystem implements AttributeManager {
+    private Map<String, AttributeBaseSource> attributeBases = new HashMap<>();
+
+    @Override
+    public void registerAttribute(String attributeName, AttributeBaseSource attributeBaseSource) {
+        attributeBases.put(attributeName, attributeBaseSource);
+    }
+
+    @Override
+    public float getAttribute(EntityRef entity, String attributeName) {
+        final AttributeBaseSource attributeBaseSource = attributeBases.get(attributeName);
+        float attributeBaseValue = 0;
+        if (attributeBaseSource != null) {
+            attributeBaseValue = attributeBaseSource.getAttributeBase(entity);
+        }
+        GetAttributeValue attributeEvent = new GetAttributeValue(attributeBaseValue);
+        entity.send(attributeEvent);
+        return attributeEvent.getResultValue();
+    }
+}
---
+++ src/main/java/org/terasology/rpgAttributes/system/RpgAttributesSystem.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2014 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.rpgAttributes.system;
+
+import org.terasology.attribute.AttributeManager;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.logic.characters.GetMaxSpeedEvent;
+import org.terasology.logic.health.BeforeDamagedEvent;
+import org.terasology.registry.In;
+import org.terasology.rpgAttributes.RPG;
+import org.terasology.rpgAttributes.component.AgilityComponent;
+import org.terasology.rpgAttributes.component.ComponentAttributeBaseSource;
+import org.terasology.rpgAttributes.component.StrengthComponent;
+
+@RegisterSystem
+public class RpgAttributesSystem extends BaseComponentSystem {
+    @In
+    private AttributeManager attributeManager;
+
+    @Override
+    public void preBegin() {
+        attributeManager.registerAttribute(RPG.STRENGTH, new ComponentAttributeBaseSource(StrengthComponent.class, 100));
+        attributeManager.registerAttribute(RPG.AGILITY, new ComponentAttributeBaseSource(AgilityComponent.class, 100));
+    }
+
+    @ReceiveEvent
+    public void doingDamage(BeforeDamagedEvent event, EntityRef damageTarget) {
+        final float strength = attributeManager.getAttribute(event.getInstigator(), RPG.STRENGTH);
+        if (strength != 100) {
+            event.multiply(strength/100f);
+        }
+    }
+
+    @ReceiveEvent
+    public void impactOnSpeed(GetMaxSpeedEvent event, EntityRef entity) {
+        final float agility = attributeManager.getAttribute(entity, RPG.AGILITY);
+        if (agility != 100) {
+            event.multiply(agility/100f);
+        }
+    }
+}
