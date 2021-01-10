536f75193989c66a949b3f2d71c5194f5d95d9b9
Josephine Rueckert <jd.rueckert@googlemail.com>
2021-01-10 20:07:10 +0100

    feat(JOML): migrate WoodAndStone module

--- src/main/java/org/terasology/herbalism/generator/HerbAGrowthDefinition.java
+++ src/main/java/org/terasology/herbalism/generator/HerbAGrowthDefinition.java
@@ -1,27 +1,11 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.herbalism.generator;
 
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import org.terasology.anotherWorld.LocalParameters;
+import org.joml.Vector3i;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.genome.component.GenomeComponent;
 import org.terasology.gf.grass.ReplaceBlockGrowthDefinition;
-import org.terasology.math.geom.Vector3i;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.world.BlockEntityRegistry;
 import org.terasology.world.WorldProvider;
@@ -39,18 +23,8 @@ public class HerbAGrowthDefinition extends ReplaceBlockGrowthDefinition {
         super(ID, Arrays.asList(
                         new BlockUri("WoodAndStone:HerbGrowA"), new BlockUri("WoodAndStone:HerbGrownA"), new BlockUri("CoreAssets:DeadBush")),
                 50 * 1000, 200 * 1000,
-                new Predicate<LocalParameters>() {
-                    @Override
-                    public boolean apply(LocalParameters input) {
-                        return input.getHumidity() > 0.2f && input.getTemperature() > 15f;
-                    }
-                },
-                new Function<LocalParameters, Float>() {
-                    @Override
-                    public Float apply(LocalParameters input) {
-                        return 0.2f * input.getHumidity();
-                    }
-                }
+                input -> input.getHumidity() > 0.2f && input.getTemperature() > 15f,
+                input -> 0.2f * input.getHumidity()
         );
     }
 
