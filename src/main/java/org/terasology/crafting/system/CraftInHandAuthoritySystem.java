8c1c37ab877e3610f307ee7ea7983beb02d3c1ee
Michael Pollind <polli104@mail.chapman.edu>
2021-01-10 11:12:43 -0800

    feat(JOML): migrate CraftInHandAuthoritySystem (#68)

--- src/main/java/org/terasology/crafting/system/CraftInHandAuthoritySystem.java
+++ src/main/java/org/terasology/crafting/system/CraftInHandAuthoritySystem.java
@@ -15,6 +15,7 @@
  */
 package org.terasology.crafting.system;
 
+import org.joml.Vector3f;
 import org.terasology.crafting.event.UserCraftInHandRequest;
 import org.terasology.crafting.system.recipe.hand.CraftInHandRecipe;
 import org.terasology.entitySystem.entity.EntityManager;
@@ -51,7 +52,7 @@ public class CraftInHandAuthoritySystem extends BaseComponentSystem {
                 if (result != null) {
                     EntityRef resultEntity = result.craft(character, event.getCount());
                     if (resultEntity.exists()) {
-                        resultEntity.send(new DropItemEvent(character.getComponent(LocationComponent.class).getWorldPosition()));
+                        resultEntity.send(new DropItemEvent(character.getComponent(LocationComponent.class).getWorldPosition(new Vector3f())));
                     }
                 }
             }
