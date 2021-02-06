102fc0dfdbfc45fb4ada1dfb196ca44860f26ffe
Tobias Nett <skaldarnar@googlemail.com>
2021-02-06 14:15:29 +0100

    feat(JOML): migrate `BlockComponent`
    Depends on MovingBlocks/Terasology#4470

--- src/main/java/org/terasology/was/system/ModifyBlockDestruction.java
+++ src/main/java/org/terasology/was/system/ModifyBlockDestruction.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.was.system;
 
 import org.terasology.entitySystem.entity.EntityRef;
@@ -65,7 +52,7 @@ public class ModifyBlockDestruction extends BaseComponentSystem {
     public void preventPlayerFromDestroyingBasicBlocksByHand(BeforeDamagedEvent event, EntityRef blockEntity) {
         BlockComponent blockComponent = blockEntity.getComponent(BlockComponent.class);
         if (blockComponent != null && event.getInstigator().hasComponent(CharacterComponent.class)) {
-            Block block = blockComponent.getBlock();
+            Block block = blockComponent.block;
 
             if (exceptions.contains(block.getURI())) {
                 return;
